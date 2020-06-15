be0a4c2994abf42124224be9b2577a05046fe40b
nemens1s <ilja.nikolski@hotmail.com>
2020-06-15 15:10:57 +0300

    Cinemas are now represented as enums, every enum has two fields, one represents API for retrieving schedule for specific date and second API allows to get dates for which schedule is available

---
+++ src/main/java/cinep/app/cinep/model/Cinema.java
@@ -0,0 +1,28 @@
+package cinep.app.cinep.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Cinema {
+
+
+    FORUM("https://www.forumcinemas.ee/xml/Schedule/?area=1008&dt=",
+            "https://www.forumcinemas.ee/xml/ScheduleDates/"),
+    APOLLO("https://www.apollokino.ee/xml/Schedule/?dt=",
+            "https://www.apollokino.ee/xml/ScheduleDates/"),
+    T1("https://api.cinamonkino.com/xml/Schedule/?id=9989&lang=en-US", ""),
+    KOSMOS("https://api.cinamonkino.com/xml/Schedule/?id=9999&lang=en-US", ""),
+    VIIMSI("http://www.viimsikino.ee/xml/Schedule/?dt=","http://www.viimsikino.ee/xml/ScheduleDates/"),
+    ARTIS("https://www.kino.ee/xml/Schedule/?dt=","https://www.kino.ee/xml/ScheduleDates/");
+
+    private final String scheduleUrl;
+    private final String datesUrl;
+
+
+    Cinema(String scheduleUrl, String dateUrl) {
+        this.scheduleUrl = scheduleUrl;
+        this.datesUrl = dateUrl;
+    }
+
+
+}
