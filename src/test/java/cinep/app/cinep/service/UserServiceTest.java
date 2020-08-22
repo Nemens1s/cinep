342536c80ffdf05a385ea0302a6ffcf584953b8f
nemens1s <ilja.nikolski@hotmail.com>
2020-08-22 12:52:58 +0300

    Added tests

--- src/test/java/cinep/app/cinep/service/UserServiceTest.java
+++ src/test/java/cinep/app/cinep/service/UserServiceTest.java
@@ -6,11 +6,6 @@ import cinep.app.cinep.dto.UserDto;
 import cinep.app.cinep.exceptions.MovieAlreadyInBookmarksException;
 import cinep.app.cinep.exceptions.MyBadRequestException;
 import cinep.app.cinep.exceptions.UserAlreadyInDatabaseException;
-import cinep.app.cinep.exceptions.UserNotFoundException;
-import cinep.app.cinep.model.Movie;
-import cinep.app.cinep.model.User;
-import cinep.app.cinep.repository.UserRepository;
-import cinep.app.cinep.security.SecurityConfig;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,17 +13,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -109,6 +100,36 @@ public class UserServiceTest {
         userService.removeBookmark(dto, "First User");
     }
 
+    @Test (expected = MyBadRequestException.class)
+    public void testRegisterNameNull () throws UserAlreadyInDatabaseException {
+        UserDto dto = new UserDto();
+        dto.setPassword("test");
+        userService.register(dto);
+    }
+
+    @Test (expected = MyBadRequestException.class)
+    public void testRegisterNameEmptyString () throws UserAlreadyInDatabaseException {
+        UserDto dto = new UserDto();
+        dto.setUsername("");
+        dto.setPassword("test");
+        userService.register(dto);
+    }
+
+    @Test (expected = MyBadRequestException.class)
+    public void testRegisterPasswordNull () throws UserAlreadyInDatabaseException {
+        UserDto dto = new UserDto();
+        dto.setUsername("test");
+        userService.register(dto);
+    }
+
+    @Test (expected = MyBadRequestException.class)
+    public void testRegisterUserPasswordEmpty () throws UserAlreadyInDatabaseException {
+        UserDto dto = new UserDto();
+        dto.setUsername("test");
+        dto.setPassword("");
+        userService.register(dto);
+    }
+
     @Test (expected = UserAlreadyInDatabaseException.class)
     public void testRegisterUserAlreadyExists () throws UserAlreadyInDatabaseException {
         UserDto dto = new UserDto();
@@ -116,7 +137,6 @@ public class UserServiceTest {
         dto.setPassword("qwerty");
         userService.register(dto);
     }
-
 
     @Test
     public void testRegisterAndThenLoginSuccess () throws UserAlreadyInDatabaseException {
@@ -127,90 +147,4 @@ public class UserServiceTest {
         LoginDetails details = userService.login(dto);
         assertTrue(details.getRoles().stream().anyMatch(role -> role.equals("ROLE_USER")));
     }
-//
-//    @Test(expected = UserNotFoundException.class)
-//    @Transactional
-//    public void insertIntoBookmarksUserNotFoundException() throws UserNotFoundException, MovieAlreadyInBookmarksException {
-//        userService.addToBookmarks(100L, "Not found");
-//    }
-//
-//    @Test(expected = MovieAlreadyInBookmarksException.class)
-//    @Transactional
-//    public void insertIntoBookmarksAlreadyInBookmarksException() throws MovieAlreadyInBookmarksException, UserNotFoundException {
-//        userService.addToBookmarks(100L, "First User");
-//        userService.addToBookmarks(100L, "First User");
-//    }
-//
-//    @Test(expected = UserNotFoundException.class)
-//    @Transactional
-//    public void getBookmarksFromNotExistingUser() throws UserNotFoundException {
-//        userService.getBookmarks("Not existing");
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void getBookmarksFromUserWithoutBookmarks() throws UserNotFoundException {
-//        Set<Movie> movieDtos = userService.getBookmarks("Seventh User");
-//        assertEquals(0, movieDtos.size());
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void getBookmarksUserHasMultipleBookmarks() throws UserNotFoundException {
-//        Set<Movie> movieDtos = userService.getBookmarks("First User");
-//        assertEquals(2, movieDtos.size());
-//    }
-//
-//
-//    @Test
-//    @Transactional
-//    public void insertIntoBookMarksSuccess() throws UserNotFoundException, MovieAlreadyInBookmarksException {
-//        assertEquals(1, userService.addToBookmarks(1L, "Seventh User").size());
-//    }
-//
-//    @Test
-//    public void registerSuccess() throws UserAlreadyInDatabaseException {
-//        UserDto userDto = new UserDto();
-//        userDto.setUsername("test");
-//        userDto.setPassword("test1234");
-//        HttpStatus httpStatus = userService.register(userDto);
-//        assertEquals(HttpStatus.CREATED, httpStatus);
-//    }
-//    @Test(expected = MyBadRequestException.class)
-//    public void registerSuccessNameIsNullThrowsException() throws UserAlreadyInDatabaseException {
-//        UserDto userDto = new UserDto();
-//        userDto.setPassword("test");
-//        userService.register(userDto);
-//    }
-//
-//    @Test(expected = MyBadRequestException.class)
-//    public void registerPasswordIsNullThrowsException() throws UserAlreadyInDatabaseException {
-//        UserDto userDto = new UserDto();
-//        userDto.setUsername("Test");
-//        userService.register(userDto);
-//    }
-//    @Test(expected = MyBadRequestException.class)
-//    public void registerUsernameEmptyStringThrowsException() throws UserAlreadyInDatabaseException {
-//        UserDto userDto = new UserDto();
-//        userDto.setUsername("");
-//        userDto.setPassword("test");
-//        userService.register(userDto);
-//    }
-//
-//    @Test(expected = MyBadRequestException.class)
-//    public void registerPasswordEmptyStringThrowsException() throws UserAlreadyInDatabaseException {
-//        UserDto userDto = new UserDto();
-//        userDto.setUsername("Test");
-//        userDto.setPassword("");
-//        userService.register(userDto);
-//    }
-//    @Test(expected = UserAlreadyInDatabaseException.class)
-//    public void registerUserAlreadyExists() throws UserAlreadyInDatabaseException {
-//        UserDto userDto = new UserDto();
-//        userDto.setUsername("Test");
-//        userDto.setPassword("Test");
-//        HttpStatus status = userService.register(userDto);
-//        assertEquals(HttpStatus.CREATED, status);
-//        userService.register(userDto);
-//    }
 }
